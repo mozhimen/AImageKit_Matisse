cd82d069896bb97705e9a49b3ebea45d3963c7fa
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-06-20 20:51:10 +0800

    0620

--- imagek_matisse/src/main/java/com/mozhimen/imagek/matisse/widgets/PreviewViewPager.kt
+++ imagek_matisse/src/main/java/com/mozhimen/imagek/matisse/widgets/PreviewViewPager.kt
@@ -5,26 +5,18 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.viewpager.widget.ViewPager
+import com.mozhimen.xmlk.pagerk.PagerKDisScroll
 import it.sephiroth.android.library.imagezoom.ImageViewTouch
 
 /**
  * Created by liubo on 2018/9/10.
  */
-class PreviewViewPager(context: Context, attributes: AttributeSet) : ViewPager(context, attributes) {
+class PreviewViewPager(context: Context, attributes: AttributeSet) : PagerKDisScroll(context, attributes) {
 
     override fun canScroll(v: View?, checkV: Boolean, dx: Int, x: Int, y: Int): Boolean {
         if (v is ImageViewTouch) {
             return v.canScroll(dx) || super.canScroll(v, checkV, dx, x, y)
         }
         return super.canScroll(v, checkV, dx, x, y)
-    }
-
-    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-        return try {
-            super.onInterceptTouchEvent(ev)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
     }
 }
